2b6efccc0871948ebd5bb9d3c8cfa19501f6ae77
MonseigneurDuez <147377272+MonseigneurDuez@users.noreply.github.com>
2023-10-13 09:23:03 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+class pokemon {
+  constructor(name,attack, défense,hp,luck) {
+     this.name = name
+     this.attack = attack
+     this.défense = défense
+     this.hp = hp
+     this.luck = luck
+  }
+}
+
+let ronflex = new pokemon("ronflex", 12,31,32,43)
+let leviator = new pokemon("leciator",32,34,47,73)
+ronflex.attackpokemon(leviator)
+
+while 
+ronflex.attackpokemon(leviator)
