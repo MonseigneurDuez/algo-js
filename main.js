d087da5feab5ed8b55ab4d81a4d8129bbc3b593b
MonseigneurDuez <147377272+MonseigneurDuez@users.noreply.github.com>
2023-10-13 10:20:22 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,13 @@
-class pokemon {
-  constructor(name,attack, défense,hp,luck) {
-     this.name = name
-     this.attack = attack
-     this.défense = défense
-     this.hp = hp
-     this.luck = luck
-  }
+let users = ["Stéphanie", "Gaïa", "Etienne", "Michel", "Roberto", "Julie"]
+
+function countCharacter(value) {
+    return value.length
 }
 
-let ronflex = new pokemon("ronflex", 12,31,32,43)
-let leviator = new pokemon("leciator",32,34,47,73)
-ronflex.attackpokemon(leviator)
-
-while 
-ronflex.attackpokemon(leviator)
+users.forEach(users => {
+    if (countCharacter(users) < 5) {
+        console.log("c'est un prénom long de plus de 5 lettres.")
+    } else {
+        console.log("ce n'est pas du tout un prénom long.")
+    }
+  });
